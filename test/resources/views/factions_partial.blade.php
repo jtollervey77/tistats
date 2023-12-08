dbbba2d63e70bef6dbb4867e20dc898a9a45a002
Jon <Jon@192.168.0.4>
2023-12-08 00:16:14 +0000

    css tweaks

--- test/resources/views/factions_partial.blade.php
+++ test/resources/views/factions_partial.blade.php
@@ -18,8 +18,7 @@
 	<div class="box faction">
 		<a href="/faction/{{ $datum->getURLName() }}">
 		<div class="faction-img img-{{ $datum->getURLName() }}"></div>
-		<p>{{ $datum->getName() }}</p>
-		<br/>
+		<p>{{ $datum->getName() }}</p>		
 		<p>Current win weighting : <span class="<?= $datum->getDeviation()<=0?'red':'green' ?>">{{ $datum->getDeviation() }}</span> </p>
 		</a>		
 	</div>	
